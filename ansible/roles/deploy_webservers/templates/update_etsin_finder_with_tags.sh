27cc47ed2f290d2a4aa4290a5b4a38b8ed4af57f
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-10-12 16:45:12 +0300

    Add first ideas about how the deployment to staging and production should go. Add more questions and todos for frontend builds. Separate app_config to its separate task so it can be used in staging and prod deployments as well.

---
+++ ansible/roles/deploy_webservers/templates/update_etsin_finder_with_tags.sh
@@ -0,0 +1,16 @@
+if [ "$#" -ne 1 ]
+then
+  echo "Give exactly one tag name for etsin-finder"
+  echo "Example: '$0 v0.1.0'"
+  exit 2
+fi
+
+if [ "$USER" != "{{ app_user }}" ]; then
+    echo "Run this as {{ app_user }}"
+    su {{ app_user }}
+fi
+
+source {{ python_virtualenv_path }}/bin/activate
+cd {{ app_base_path }}
+
+git fetch --all --tags --prune && git checkout $1
