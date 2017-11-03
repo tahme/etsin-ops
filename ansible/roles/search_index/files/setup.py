155c0c9dd8b5350d68244cf71bc3242c6153ce33
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-11-03 12:20:19 +0200

    Update setup.py

--- ansible/roles/search_index/files/setup.py
+++ ansible/roles/search_index/files/setup.py
@@ -1,13 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='etsin_search_index',
-    packages=['etsin_search_index'],
-    include_package_data=True,
+    description='Etsin finder search index related scripts',
+    author='CSC - IT Center for Science Ltd.',
+    packages=find_packages(exclude=['scripts']),
     setup_requires=[
         'pytest-runner'
-    ],
-    tests_require=[
-        'pytest'
     ]
 )
