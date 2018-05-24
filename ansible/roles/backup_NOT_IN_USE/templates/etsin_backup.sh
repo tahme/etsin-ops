58a64fd193e30b57691e5ca12ce625e3a0ac2b56
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-05-24 15:54:53 +0300

    Remove db related things from ansible script since it is not needed. Add production secrets to gitignore and some staging stuff as well.

--- ansible/roles/backup_NOT_IN_USE/templates/etsin_backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-TIMESTAMP=$(date +%Y%m%d%H%M%S)
-pg_dump --format=custom $ETSIN_DATABASE_NAME -f {{ etsin_db_backup_archive_path }}/etsin_db_{{ deployment_environment_id }}_backup_${TIMESTAMP}.dump
--- ansible/roles/backup_NOT_IN_USE/templates/etsin_backup_sync.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-nextcloudcmd -u {{ etsin_backup_username }} -p '{{ etsin_backup_password }}' {{ etsin_db_backup_archive_path }} https://kannu.csc.fi/remote.php/webdav/ETSIN_DB_BACKUP
