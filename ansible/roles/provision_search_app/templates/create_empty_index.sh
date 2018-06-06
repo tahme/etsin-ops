3652079c36c0cd718f30a7661e4a6630a2bd0db5
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-06-06 21:25:04 +0300

    - Add spacewalk role and playbook for it - Add update nginx playbook - Use es 5.6.9 - Change app base directory - Refactor os-base role - Write cronjob based reindexing ops to log file - Update python role - etsin-user no longer has sudo rights

--- ansible/roles/provision_search_app/templates/create_empty_index.sh
+++ ansible/roles/provision_search_app/templates/create_empty_index.sh
@@ -6,6 +6,7 @@ if [ "$USER" != "{{ app_user }}" ]; then
     exit 1
 fi
 
+sudo chown -R {{ app_user }}:etsin {{ search_app_log_base_path }}
 source /srv/etsin/pyenv/bin/activate
 cd {{ search_app_base_path }}
 python create_empty_index.py
