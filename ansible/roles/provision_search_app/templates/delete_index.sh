411078fe252067d35872ef55760e539da098f363
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2018-06-11 09:56:04 +0300

    Allow etsin-user to run rabbitmq-consumer service.

--- ansible/roles/provision_search_app/templates/delete_index.sh
+++ ansible/roles/provision_search_app/templates/delete_index.sh
@@ -6,7 +6,6 @@ if [ "$USER" != "{{ app_user }}" ]; then
     exit 1
 fi
 
-sudo chown -R {{ app_user }}:etsin {{ search_app_log_base_path }}
 source {{ python_virtualenv_path }}/bin/activate
 cd {{ search_app_base_path }}
 python delete_index.py
